b26e4763acee0ec29ca57506e9a1c77f0695ffbf
acobianiregui <anton.cobian.iregui@icloud.com>
2026-04-03 15:55:33 -0400

    Workflow check

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def make_random_data(n_samples=200):
 ######################################################
 #MAIN
 X,y=make_random_data()
-
+d=
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=TEST_SIZE,random_state=RANDOM_STATE)
 
 pipeline=create_LR_pipeline()
